4231f05d3d91f88d149e3d2a2644f0c1a508e949
stomskij <56798121+stomskij@users.noreply.github.com>
2019-10-20 20:51:37 +0300

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -15,7 +15,7 @@ function send_mail()
 		$mail = new PHPMailer();
 		$mail->From = "киндер-подарок.рф";
 		$mail->FromName = $_REQUEST['name'];
-		$mail->AddAddress('kinder-united@mail.ru');
+		$mail->AddAddress('stomskij@gmail.com');
 		$mail->IsHTML(true);
 		$mail->Subject = "E-mail с сайта";
 
